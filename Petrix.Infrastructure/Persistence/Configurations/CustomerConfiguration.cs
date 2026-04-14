9eb5811cf3daa1597321ea68a0ac36411195741e
George <george.conti@datapar.com.br>
2026-04-14 11:03:43 -0300

    refactor: add unique to documentnumber and remove document formatter

--- Petrix.Infrastructure/Persistence/Configurations/CustomerConfiguration.cs
+++ Petrix.Infrastructure/Persistence/Configurations/CustomerConfiguration.cs
@@ -1,25 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Petrix.Domain.Entities;
-using Petrix.Application.Common;
-
 namespace Petrix.Infrastructure.Persistence.Configurations
 {
     public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
     {
         public void Configure(EntityTypeBuilder<Customer> builder)
         {
+            builder.HasIndex(u => u.DocumentNumber).IsUnique();
+
             builder.Property(x => x.Name)
                     .IsRequired()
                     .HasMaxLength(150);
             builder.Property(x => x.DocumentNumber)
                    .HasMaxLength(14)
                    .IsRequired()
-                   .HasColumnType("char(14)")
-                    .HasConversion(
-                        v => DocumentNumberFormatter.FormatCpf(v ?? string.Empty),
-                        v => v
-                    );
+                   .HasColumnType("char(14)");
+                    
             builder.Property(x => x.Email)
                    .HasMaxLength(255);
             builder.Property(x => x.Phone)
