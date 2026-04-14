f9119423c0b4c25bb4bd3ff774d43a81c2b547e9
George <george.conti@datapar.com.br>
2026-04-14 13:24:36 -0300

    chore: add migrations

---
+++ Petrix.Infrastructure/Migrations/20260414161746_AddPetCustomerRelationship.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Petrix.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPetCustomerRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CustomerId",
+                table: "Pets",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_CustomerId",
+                table: "Pets",
+                column: "CustomerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pets_Customers_CustomerId",
+                table: "Pets",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pets_Customers_CustomerId",
+                table: "Pets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pets_CustomerId",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "Pets");
+        }
+    }
+}
