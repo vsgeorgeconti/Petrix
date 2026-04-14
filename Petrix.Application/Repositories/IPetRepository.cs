c7570b17280ac618852a846fa9fa65187b2b1e13
George <george.conti@datapar.com.br>
2026-04-14 12:47:36 -0300

    fix: adjust on interface methods

--- Petrix.Application/Repositories/IPetRepository.cs
+++ Petrix.Application/Repositories/IPetRepository.cs
@@ -4,10 +4,10 @@ namespace Petrix.Application.Repositories
 {
     public interface IPetRepository
     {
-        Task<Pet?> GetByIdAsyc(Guid id);
+        Task<Pet?> GetByIdAsync(Guid id);
         Task<IEnumerable<Pet>>GetAllAsync();
         Task AddAsync(Pet pet);
-        void Upload(Pet pet);
+        void Update(Pet pet);
         void Delete(Pet pet);
         Task SaveChangesAsync();
     }
