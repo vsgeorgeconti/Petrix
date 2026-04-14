a453ec83d5a9555ae01a99e124ec2a5b05ace17b
George <george.conti@datapar.com.br>
2026-04-14 18:20:37 -0300

    refactor: add getbydocumentnumberasync

--- Petrix.Infrastructure/Persistence/Repositories/CustomerRepository.cs
+++ Petrix.Infrastructure/Persistence/Repositories/CustomerRepository.cs
@@ -14,6 +14,9 @@ namespace Petrix.Infrastructure.Persistence.Repositories
         public async Task AddAsync(Customer customer) => await _context.Customers.AddAsync(customer);
         public void Delete(Customer customer) => _context.Customers.Remove(customer);
         public async Task<IEnumerable<Customer>> GetAllAsync() => await _context.Customers.AsNoTracking().ToListAsync();
+
+        public async Task<Customer?> GetByDocumentNumberAsync(string documentNumber) => await _context.Customers.AsNoTracking().Where(x => x.DocumentNumber == documentNumber).FirstOrDefaultAsync();
+
         public async Task<Customer?> GetByIdAsync(Guid id) => await _context.Customers.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
         public async Task SaveChangesAsync() => await _context.SaveChangesAsync();
         public void Update(Customer customer) => _context.Customers.Update(customer);
