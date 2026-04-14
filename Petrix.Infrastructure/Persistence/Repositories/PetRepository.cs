c2d0f9e247b1c930f7a8e0fa1ccbb5e1fed5d56b
George <george.conti@datapar.com.br>
2026-04-14 11:32:24 -0300

    feat: add pet repository implementation

---
+++ Petrix.Infrastructure/Persistence/Repositories/PetRepository.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Petrix.Application.Repositories;
+using Petrix.Domain.Entities;
+
+namespace Petrix.Infrastructure.Persistence.Repositories
+{
+    public class PetRepository : IPetRepository
+    {
+        private readonly AppDbContext _context;
+
+        public PetRepository(AppDbContext context)
+        {
+            this._context = context;
+        }
+        public async Task AddAsync(Pet pet) => await _context.Pets.AddAsync(pet);
+
+        public void Delete(Pet pet) => _context.Pets.Remove(pet);
+
+        public async Task<IEnumerable<Pet>> GetAllAsync() => await _context.Pets.AsNoTracking().ToListAsync();
+
+        public async Task<Pet?> GetByIdAsyc(Guid id) => await _context.Pets.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+
+        public async Task SaveChangesAsync() => await _context.SaveChangesAsync();
+
+        public void Upload(Pet pet) => _context.Update(pet);
+    }
+}
