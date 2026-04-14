45acd9751c96b3662c29ce454fbcbea42547ce6e
George <george.conti@datapar.com.br>
2026-04-14 11:05:21 -0300

    chore(migrations): add unique constraint on Customer DocumentNumber

---
+++ Petrix.Infrastructure/Migrations/20260414135440_AddUniqueIndexToCustomerDocumentNumber.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Petrix.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndexToCustomerDocumentNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_DocumentNumber",
+                table: "Customers",
+                column: "DocumentNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_DocumentNumber",
+                table: "Customers");
+        }
+    }
+}
