cce0c373c3260e1b3c151a37a9e121afdf500328
George <george.conti@datapar.com.br>
2026-04-16 20:18:19 -0300

    feat(web): add customer form

---
+++ petrix-web/src/app/features/customer/pages/customer-form/customer-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-form',
+  imports: [],
+  templateUrl: './customer-form.html',
+  styleUrl: './customer-form.css',
+})
+export class CustomerFormComponent {}
